3e7aa287273d9f1488d26d0e4f9136563a887c38
Tore Heliks Van Dahl <toredah@gmail.com>
2026-05-24 09:03:03 +0200

    New hidden token version

--- api/netatmo.js
+++ api/netatmo.js
@@ -1,13 +1,13 @@
-// export default async function handler(req, res) {
-//   const token = process.env.NETATMO_TOKEN;
-//   const response = await fetch("https://api.netatmo.com/api/getstationsdata", {
-//     headers: { "Authorization": "Bearer " + token }
-//   });
-//   const data = await response.json();
-//   res.status(200).json(data);
-// }
-
 export default async function handler(req, res) {
   const token = process.env.NETATMO_TOKEN;
-  res.status(200).json({ tokenFound: !!token, tokenLength: token?.length });
+  const response = await fetch("https://api.netatmo.com/api/getstationsdata", {
+    headers: { "Authorization": "Bearer " + token }
+  });
+  const data = await response.json();
+  res.status(200).json(data);
 }
+
+// export default async function handler(req, res) {
+//   const token = process.env.NETATMO_TOKEN;
+//   res.status(200).json({ tokenFound: !!token, tokenLength: token?.length });
+// }
