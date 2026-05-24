a4dfadc45aa0b5a9c9244022c472860be07ce654
Tore Heliks Van Dahl <toredah@gmail.com>
2026-05-24 08:32:57 +0200

    token from variable

---
+++ api/netatmo.js
@@ -0,0 +1,8 @@
+export default async function handler(req, res) {
+  const token = process.env.NETATMO_TOKEN;
+  const response = await fetch("https://api.netatmo.com/api/getstationsdata", {
+    headers: { "Authorization": "Bearer " + token }
+  });
+  const data = await response.json();
+  res.status(200).json(data);
+}
